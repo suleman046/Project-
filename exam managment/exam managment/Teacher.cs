c275e5b7d4a971968765cc46fa5fa2676a6cc3e1
suleman046 <156238301+suleman046@users.noreply.github.com>
2024-01-13 22:27:23 +0500

    Add files via upload

---
+++ exam managment/exam managment/Teacher.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace exam_managment
+{
+    public partial class Teacher : Form
+    {
+        public Teacher()
+        {
+            InitializeComponent();
+        }
+
+        private void Teacher_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
